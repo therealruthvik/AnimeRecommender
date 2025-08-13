7b6e45f3cf8b266a26fe322ab5109b8256509c17
Ruthvik Garlapati <garlapati.r@northeastern.edu>
2025-08-12 19:24:24 -0700

    app ran in local

--- src/recommender.py
+++ src/recommender.py
@@ -1,20 +1,20 @@
-from langchain.chains import RetirvalQA
+from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
 from src.prompt_template import get_anime_prompt
 
 class AnimeRecommender:
-    def __init__(self, retriever, api_key: str, model_name: str):
-        self.llm = ChatGroq(api_key=api_key, model=model_name, temperature=0)
+    def __init__(self,retriever,api_key:str,model_name:str):
+        self.llm = ChatGroq(api_key=api_key,model=model_name,temperature=0)
         self.prompt = get_anime_prompt()
 
-        self.qa_chain = RetirvalQA.from_chain_type(
+        self.qa_chain = RetrievalQA.from_chain_type(
             llm = self.llm,
             chain_type = "stuff",
             retriever = retriever,
             return_source_documents = True,
-            chain_type_kwargs = {"prompt": self.prompt}
+            chain_type_kwargs = {"prompt":self.prompt}
         )
 
-    def getRecommendation(self,query: str):
-        result = self.qa_chain({"query": query})
+    def get_recommendation(self,query:str):
+        result = self.qa_chain({"query":query})
         return result['result']
